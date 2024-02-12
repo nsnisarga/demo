10ae09ce26921933ff9945af6cd39855cc93c9a1
nsnisarga <nisarganayak02@gmail.com>
2024-02-12 17:57:11 +0530

    done

---
+++ series.py
@@ -0,0 +1,21 @@
+  
+# Python code to find smallest K-digit  
+# number divisible by X
+ 
+def sumOfSeries(num):
+     
+    # Computing MAX
+    res = 0
+    fact = 1
+     
+    for i in range(1, num+1):
+        fact *= i
+        res = res + (i/ fact)
+         
+    return res
+     
+ 
+n = 5
+print("Sum: ", sumOfSeries(n))
+ 
+# Code contributed by Mohit Gupta_OMG <(0_o)>
